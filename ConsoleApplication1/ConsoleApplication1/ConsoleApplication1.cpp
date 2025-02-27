09194249538ed8f944df8bcd9f4443db1d5074c6
Wandestes <gilua5568@gmail.com>
2025-02-27 11:06:38 +0200

    Start
    Create project and adding input validation function

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+// Функція для перевірки правильності вводу
+double getValidInput(const string& prompt) {
+    double value;
+    while (true) {
+        cout << prompt;
+        cin >> value;
+
+        if (cin.fail()) {
+            cin.clear(); // Очищуємо стан помилки
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Пропускаємо неправильний ввід
+            cout << "Error. Expected a valid real number." << endl;
+        }
+        else {
+            return value;
+        }
+    }
+}
+
